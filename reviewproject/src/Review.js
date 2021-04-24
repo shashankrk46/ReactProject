1cb0073a87db574d99eeedce9a8c5fec18b9e24e
shashankrk46 <shashankrk46@gmail.com>
2021-04-24 23:20:01 +0530

    added button functionality

--- reviewproject/src/Review.js
+++ reviewproject/src/Review.js
@@ -6,6 +6,37 @@ const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
 
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0
+    }
+    if (number < 0) {
+      return people.length - 1
+    }
+    return number
+
+  }
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1
+      return checkNumber(newIndex)
+    })
+  }
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1
+      return checkNumber(newIndex)
+    })
+  }
+  const randomPerson = () => {
+    let random = Math.floor(Math.random() * people.length)
+    if (random === index) {
+      return random = index + 1
+    }
+    console.log(random)
+    setIndex(checkNumber(random))
+  }
 
   return (
     <article className="review">
@@ -19,14 +50,14 @@ const Review = () => {
       <p className="job">{job}</p>
       <p className="info">{text}</p>
       <div className="button-container">
-        <button onClick={() => setIndex(index - 1)} className='prev-btn'>
+        <button onClick={prevPerson} className='prev-btn'>
           <FaChevronLeft />
         </button>
-        <button onClick={() => setIndex(index + 1)} className='next-btn'>
+        <button onClick={nextPerson} className='next-btn'>
           <FaChevronRight />
         </button>
       </div>
-      <button className='random-btn'>
+      <button onClick={randomPerson} className='random-btn'>
         Surprise me
         </button>
     </article>
